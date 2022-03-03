5c27f3c659d8363de264a862238a6f5f000ebd63
William Dias <smdwilliam89@gmail.com>
2022-03-03 14:31:21 +0000

    Projeto Criado e Comment component criado

---
+++ src/components/Comment.jsx
@@ -0,0 +1,12 @@
+import './Comment.css';
+
+const Comment = () => {
+  return (
+    <div className="comment">
+      <h2>William Dias</h2>
+      <p>Front-end developer</p>
+    </div>
+  );
+};
+
+export default Comment;
