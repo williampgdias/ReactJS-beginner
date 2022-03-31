0ddee65da3efb8df7f28536fbcf7bba6282ab43c
William Dias <smdwilliam89@gmail.com>
2022-03-31 11:11:49 +0100

    Criado o estilo do Comment componente

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,15 +1,24 @@
 import "./Comment.css";
+import ProfileImg from "./profile-comment.png"
 
 const Comment = (props) => {
     const { name, email, message, date, onDeleteComment } = props;
 
     return (
         <div className="comment">
-            <h2>{ name }</h2>
-            <p>{ email }</p>
-            <p>{ message }</p>
-            <p>{ date.toString() }</p>
-            <button onClick = { onDeleteComment }>&times;</button>
+
+            <img src={ ProfileImg } alt="User avatar comment" />
+
+            <div className="content">
+
+                <h2 className="name">{ name }</h2>
+                <p className="email">{ email }</p>
+                <p className="message">{ message }</p>
+                <p className="date">{ date.toString() }</p>
+                <button className="delete" onClick = { onDeleteComment }>&times;</button>
+
+            </div>
+
         </div>
     );
 };
